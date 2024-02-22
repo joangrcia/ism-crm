8fb2cffd9c8ee51271514db27f11c5a897161e51
Juan Garcia <j.riswan321@gmail.com>
2024-02-23 06:39:33 +0700

    Modify imperium settings and dashboard_app index.html template.

--- imperium/settings.py
+++ imperium/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6hx(%3hxb&5*-)6n+8p_19z2s4kl6gf^rbrn!07&c(a94jkc&m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
